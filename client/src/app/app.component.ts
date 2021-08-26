74feb70dcf5eccdf52cdcdd70317e47333e50340
Neos21 <neos21@gmail.com>
2021-08-27 02:28:05 +0900

    Client : デザイン調整

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 
+import { AuthGuard } from './shared/auth.guard';
+
 /** App Component */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private authGuard: AuthGuard) { }
+  
+  /** ログインしているか否か */
+  public isLogined(): boolean {
+    return this.authGuard.isLogined;
+  }
+  
+  /** 「Logout」ボタン押下時 */
+  public onLogout(): void {
+    return this.authGuard.logout(true);
+  }
+}
--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -20,13 +20,12 @@ export class HomeComponent implements OnInit {
   
   /** 画面初期表示時 */
   public async ngOnInit(): Promise<void> {
-    this.setState();
     try {
-      this.setState(undefined, await this.apiFilmsService.findAll());
+      this.films = await this.apiFilmsService.findAll();
     }
     catch(error) {
       console.error('Home Component : On Init', error);
-      this.setState('Failed To Fetch Films');
+      this.errorMessage = 'Failed To Fetch Films';
     }
   }
   
@@ -39,16 +38,5 @@ export class HomeComponent implements OnInit {
   /** 子コンポーネントから伝搬されたイベント : 更新処理 */
   public async onUpdated(film: Film): Promise<void> {
     await this.apiFilmsService.save(film);
-  }
-  
-  /**
-   * 画面の状態を更新する
-   * 
-   * @param errorMessage エラーメッセージ
-   * @param films 映画情報一覧
-   */
-  private setState(errorMessage?: string, films?: Array<Film>): void {
-    this.errorMessage = errorMessage ?? '';
-    this.films = films ?? [];
   }
 }
